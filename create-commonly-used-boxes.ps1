762012b4ac7a25818a4b9e9fb3e7ee1480bfeb37
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-11-12 23:10:21 -0800

    Fix measure-output

--- create-commonly-used-boxes.ps1
+++ create-commonly-used-boxes.ps1
@@ -1,14 +1,12 @@
-Measure-Command { 
-	Measure-Command { make virtualbox/eval-win10x64-enterprise }
-	Measure-Command { make virtualbox/eval-win10x86-enterprise }
-	Measure-Command { make virtualbox/eval-win2008r2-datacenter }
-	Measure-Command { make virtualbox/eval-win2008r2-standard }
-	Measure-Command { make virtualbox/eval-win2012-standard }
-	Measure-Command { make virtualbox/eval-win2012r2-datacenter }
-	Measure-Command { make virtualbox/eval-win2012r2-standard }
-	Measure-Command { make virtualbox/eval-win7x64-enterprise }
-	Measure-Command { make virtualbox/eval-win7x86-enterprise }
-	Measure-Command { make virtualbox/eval-win81x64-enterprise }
-	Measure-Command { make virtualbox/eval-win81x86-enterprise }
-	Measure-Command { make virtualbox/eval-win8x64-enterprise }
-}
+Measure-Command { make virtualbox/eval-win10x64-enterprise | out-default }
+Measure-Command { make virtualbox/eval-win10x86-enterprise | out-default }
+Measure-Command { make virtualbox/eval-win2008r2-datacenter | out-default }
+Measure-Command { make virtualbox/eval-win2008r2-standard | out-default }
+Measure-Command { make virtualbox/eval-win2012-standard | out-default }
+Measure-Command { make virtualbox/eval-win2012r2-datacenter | out-default }
+Measure-Command { make virtualbox/eval-win2012r2-standard | out-default }
+Measure-Command { make virtualbox/eval-win7x64-enterprise | out-default }
+Measure-Command { make virtualbox/eval-win7x86-enterprise | out-default }
+Measure-Command { make virtualbox/eval-win81x64-enterprise | out-default }
+Measure-Command { make virtualbox/eval-win81x86-enterprise | out-default }
+Measure-Command { make virtualbox/eval-win8x64-enterprise | out-default }
