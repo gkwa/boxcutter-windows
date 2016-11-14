1d3bdeaea303572d78b3f16b1f93e0714edd5ad7
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-11-13 19:13:16 -0800

    Update script to test win7 until I can get winrm stable

--- create-commonly-used-boxes.ps1
+++ create-commonly-used-boxes.ps1
@@ -1,12 +1,13 @@
-Measure-Command { make virtualbox/eval-win10x64-enterprise | out-default }
-Measure-Command { make virtualbox/eval-win10x86-enterprise | out-default }
-Measure-Command { make virtualbox/eval-win2008r2-datacenter | out-default }
-Measure-Command { make virtualbox/eval-win2008r2-standard | out-default }
-Measure-Command { make virtualbox/eval-win2012-standard | out-default }
-Measure-Command { make virtualbox/eval-win2012r2-datacenter | out-default }
-Measure-Command { make virtualbox/eval-win2012r2-standard | out-default }
-Measure-Command { make virtualbox/eval-win7x64-enterprise | out-default }
-Measure-Command { make virtualbox/eval-win7x86-enterprise | out-default }
-Measure-Command { make virtualbox/eval-win81x64-enterprise | out-default }
-Measure-Command { make virtualbox/eval-win81x86-enterprise | out-default }
-Measure-Command { make virtualbox/eval-win8x64-enterprise | out-default }
+Measure-Command { make virtualbox/eval-win7x64-enterprise | out-default; email --blank-mail --subject "Packer is done with eval-win7x64-enterprise" taylor }
+Exit
+Measure-Command { make virtualbox/eval-win10x64-enterprise | out-default; email --blank-mail --subject "Packer is done with eval-win10x64-enterprise" taylor }
+Measure-Command { make virtualbox/eval-win10x86-enterprise | out-default; email --blank-mail --subject "Packer is done with eval-win10x86-enterprise" taylor }
+Measure-Command { make virtualbox/eval-win2008r2-datacenter | out-default; email --blank-mail --subject "Packer is done with eval-win2008r2-datacenter" taylor }
+Measure-Command { make virtualbox/eval-win2008r2-standard | out-default; email --blank-mail --subject "Packer is done with eval-win2008r2-standard" taylor }
+Measure-Command { make virtualbox/eval-win2012-standard | out-default; email --blank-mail --subject "Packer is done with eval-win2012-standard" taylor }
+Measure-Command { make virtualbox/eval-win2012r2-datacenter | out-default; email --blank-mail --subject "Packer is done with eval-win2012r2-datacenter" taylor }
+Measure-Command { make virtualbox/eval-win2012r2-standard | out-default; email --blank-mail --subject "Packer is done with eval-win2012r2-standard" taylor }
+Measure-Command { make virtualbox/eval-win7x86-enterprise | out-default; email --blank-mail --subject "Packer is done with eval-win7x86-enterprise" taylor }
+Measure-Command { make virtualbox/eval-win81x64-enterprise | out-default; email --blank-mail --subject "Packer is done with eval-win81x64-enterprise" taylor }
+Measure-Command { make virtualbox/eval-win81x86-enterprise | out-default; email --blank-mail --subject "Packer is done with eval-win81x86-enterprise" taylor }
+Measure-Command { make virtualbox/eval-win8x64-enterprise | out-default; email --blank-mail --subject "Packer is done with eval-win8x64-enterprise" taylor }
