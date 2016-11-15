bfc24a5e657429c94f6c0f25d088a989ee08870e
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-11-15 11:27:52 -0800

    Ping local machine 10.0.3.94 and google.com to to test connectivity

--- floppy/01-install-wget.cmd
+++ floppy/01-install-wget.cmd
@@ -9,6 +9,9 @@ title Installing wget. Please wait...
 echo temp sanity check
 ipconfig /all
 
+ping 10.0.3.94
+ping google.com
+
 if not defined WGET_URL set WGET_URL=https://eternallybored.org/misc/wget/current/wget.exe
 
 for %%i in ("%WGET_URL%") do set filename=%SystemRoot%\%%~nxi
