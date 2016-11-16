dcb3907cfad06934bffa7f5127449e0766ade760
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-11-15 18:49:34 -0800

    Ensure machine time is correct to try fix ssl download wget.exe
    Story
    https://goo.gl/P5JGuL

--- floppy/01-install-wget.cmd
+++ floppy/01-install-wget.cmd
@@ -12,6 +12,13 @@ ipconfig /all
 ping 10.0.3.94
 ping google.com
 
+tzutil /s "Pacific Standard Time"
+net stop w32time 2>NUL
+net start w32time
+net stop w32time 2>NUL
+net start w32time
+W32tm /resync /force
+
 if not defined WGET_URL set WGET_URL=http://eternallybored.org/misc/wget/current/wget.exe
 
 for %%i in ("%WGET_URL%") do set filename=%SystemRoot%\%%~nxi
