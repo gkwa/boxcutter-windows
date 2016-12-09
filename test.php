8ac6024a10b22952d7ad363da50c25c2331f50ef
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-12-08 16:32:33 -0800

    Take hints from https://github.com/mitchellh/packer/issues/2401

---
+++ test.php
@@ -0,0 +1,31 @@
+#!/usr/bin/env php
+<?php
+/*
+
+// single file
+php --file test.php -- -f eval-win81x64-enterprise.json
+
+// *.json
+ls -1 *.json | xargs -n1 -I% php --file test.php -- -f %
+git diff --ignore-all-space --ignore-blank-lines >out
+git co -- .
+git apply out
+rm out
+
+*/
+
+$shortopts = "";
+$shortopts .= "f:";  // Required value
+
+$options = getopt($shortopts);
+
+$jfile = $options["f"];
+$data = json_decode(file_get_contents($jfile), true);
+for ($i = 0; $i < count($data['builders']); $i++) {
+    if ($data['builders'][$i]['type'] == 'vmware-iso') {
+        $data['builders'][$i]['post_shutdown_delay'] = "120s";
+        $data['builders'][$i]['shutdown_timeout'] = "1h";
+    }
+}
+file_put_contents($jfile, json_encode($data));
+file_put_contents($jfile, shell_exec("jq . $jfile"));
